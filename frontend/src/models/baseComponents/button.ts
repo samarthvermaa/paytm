d2bfed73e61fd44a269ec093e7276481971d8617
Samarth Verma <samarth.verma@stationcasinos.com>
2024-09-01 22:07:25 +0530

    fix: small bug gixed

--- frontend/src/models/baseComponents/button.ts
+++ frontend/src/models/baseComponents/button.ts
@@ -4,5 +4,5 @@ export interface IButton {
   onClick?: MouseEventHandler<HTMLButtonElement>;
   className?: string;
   children?: React.ReactNode;
-  type: "submit" | "reset" | "button" | undefined;
+  type?: "submit" | "reset" | "button" | undefined;
 }
